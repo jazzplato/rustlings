dac4c167473be93733fbbf11a8b6e3dbfc11019f
Ricthofen <jazzplato@gmail.com>
2021-06-26 22:12:27 -0400

    Strings done.

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -2,13 +2,25 @@
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings1` for hints ;)
 
-// I AM NOT DONE
-
 fn main() {
-    let answer = current_favorite_color();
+    solution1();
+    solution2();
+}
+
+fn solution1() {
+    let answer = current_favorite_color1();
     println!("My current favorite color is {}", answer);
 }
 
-fn current_favorite_color() -> String {
-    "blue"
+fn current_favorite_color1() -> String {
+    String::from("blue")
+}
+
+fn solution2() {
+    let answer = current_favorite_color2();
+    println!("My current favorite color is {}", answer);
+}
+
+fn current_favorite_color2() -> String {
+    (*"blue").to_string()
 }
